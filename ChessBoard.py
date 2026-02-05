21e3bbe960915028822a72f6900b0e10b38e89ca
Sakthi Murugan C <sakthimurugankgm@gmail.com>
2026-02-05 15:25:23 +0530

    Board setup

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,10 +1,17 @@
 import pandas as pd
+from typing import List
 
-class Board(pd.DataFrame):
+from Pieces import Pawn
+
+class Board(object):
 
     def __init__(self):
-        columns = ["A", "B", "C", "D", "E", "F", "G", "H"]
-        index = ["1", "2", "3", "4", "5", "6", "7", "8"]
-        super().__init__(data=[["."] * 8 for _ in range(8)], index=index, columns=columns)
+        self.columns = ["A", "B", "C", "D", "E", "F", "G", "H"]
+        self.index = ["8", "7", "6", "5", "4", "3", "2", "1"]
+        self.board = pd.DataFrame(data=[["."] * 8 for _ in range(8)], dtype=object, index=self.index, columns=self.columns)
+        self.SetupBoard()
 
-        print(self)
+    def SetupBoard(self):
+        self.board.loc["2", :] = [Pawn("white") for _ in range(8)]
+        self.board.loc["7", :] = [Pawn("black") for _ in range(8)]
+        print(self.board)
--- Pieces.py
+++ Pieces.py
@@ -1,7 +1,71 @@
+from typing import Any, List, Literal
+from dataclasses import dataclass
 
+@dataclass
+class Pawn(object):
+    def __init__(self, color: str):
+        self.color = color
 
-class Pawn:
-    def __init__(self):
-        pass
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "P"
+        else:
+            return "p"
+        
+@dataclass
+class King(object):
+    def __init__(self, color: str):
+        self.color = color
+
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "K"
+        else:
+            return "k"
+        
+@dataclass
+class Queen(object):
+    def __init__(self, color: str):
+        self.color = color
+
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "Q"
+        else:
+            return "q"
+        
+@dataclass
+class Rook(object):
+    def __init__(self, color: str):
+        self.color = color
+
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "R"
+        else:
+            return "r"
+        
+@dataclass
+class Knight(object):
+    def __init__(self, color: str):
+        self.color = color
+
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "K"
+        else:
+            return "k"
+        
+@dataclass
+class Bisshop(object):
+    def __init__(self, color: str):
+        self.color = color
+
+    def __repr__(self) -> str:
+        if self.color.lower() in ["w", "white"]:
+            return "B"
+        else:
+            return "b"
+
 
     
